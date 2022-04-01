6022a7ca40b091b43824a1935d452d1e06f75d82
Daniel Andrade <daniel.andrade@vortigo.com.br>
2022-04-01 13:56:40 -0300

    correções model Deal

--- Routes/deals.js
+++ Routes/deals.js
@@ -1,37 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Customer = require('../model/customer');
+const Product = require('../model/product');
 const Deal = require('../model/deal');
 const config = require('../config/config');
 
 router.post('/create', async (req, res) => {
-	const { clientId, products } = req.body
-	const deal = {}
-	try {
-		const customer = await Customer.findOne({ _id: clientId })
-		deal.customer = customer
-		deal.products = []
-		var total = 0
-		for (let item of products) {
-			var product = await Product.findOne({ _id: item.id })
-			total += product.price * item.quantity
-			deal.products.push(product)
-		}
-		deal.total = total
-		await Deal.create(deal)
-		res.status(201).json(deal)
-	} catch (error) {
-		res.status(500).json({ erro: error })
-	}
+    const { clientId, products } = req.body
+    const deal = {}
+    try {
+        const customer = await Customer.findOne({ _id: clientId })
+        deal.customer = customer
+        deal.products = []
+        var dealProduct = {}
+        var total = 0
+        for (let item of products) {
+            dealProduct = {}
+            var product = await Product.findOne({ _id: item.id })
+            dealProduct.product = product
+            dealProduct.quantity = item.quantity
+            total += product.price * item.quantity
+            deal.products.push(dealProduct)
+        }
+        deal.total = total
+        await Deal.create(deal)
+        res.status(201).json(deal)
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
 });
 
 router.get('/list', async (req, res) => {
-  try {
-    const deals = await Deal.find().populate({ path: 'products', model: 'Product' }).populate('customer')
-    res.status(200).json(deals);
-  } catch (error) {
-    res.status(500).send({ error: "Erro na consulta de orçamentos" });
-  }
-});
+    try {
+      const deals = await Deal.find().populate('customer').populate('products.product')
+      res.status(200).json(deals);
+    } catch (error) {
+        console.log(error)
+      res.status(500).send({ error: "Erro na consulta de orçamentos" });
+    }
+  });
 
 router.delete('/:id', async (req, res) => {
   const id = req.params.id
@@ -44,7 +51,7 @@ router.delete('/:id', async (req, res) => {
     await Deal.deleteOne({ _id: id })
     res.status(200).json({ message: 'Orçamento removido com sucesso!' })
   } catch (error) {
-    res.status(500).json({ erro: error })
+    res.status(500).send({ message: "Erro ao excluir orçamento" })
   }
 });
 
--- model/deal.js
+++ model/deal.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Deal = mongoose.model('Deal', {
     customer: { type: Schema.Types.ObjectId, ref: 'Customer' },
-    products: [{ type: Schema.ObjectId, ref: 'Product' }],
+    products: [    { product: { type: Schema.Types.ObjectId, ref: 'Product' }, quantity: Number } ],
     total: Number,
     created: { type: Date, default: Date.now }
 });
