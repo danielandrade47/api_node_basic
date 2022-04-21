f389d160f8c0c000fa6561a9a3175ba04266b23a
Daniel Andrade <daniel.andrade63@gmail.com>
2022-04-21 18:58:15 -0300

    correção método patch para deal

--- Routes/deals.js
+++ Routes/deals.js
@@ -55,23 +55,35 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-/*
 router.patch('/:id', async (req, res) => {
   const id = req.params.id
-  const deal = await Deal.findOne({ _id: id })
-  const { clientId, products } = req.body
-  const deal = {}
   try {
-    const updatedDeal = await Deal.updateOne({ _id: id }, deal)
-    if (updatedDeal.matchedCount === 0) {
-      res.status(404).json({ message: 'Orçamento não encontrado!' })
-      return
+	var deal = await Deal.findOne({_id: id})
+	var customer = await Customer.findOne({ _id: req.body.clientId })
+	deal.customer = customer
+	deal.products = []
+    var dealProduct = {}
+    var total = 0
+    for (let item of req.body.products) {
+        dealProduct = {}
+        var product = await Product.findOne({ _id: item.id })
+        dealProduct.product = product
+        dealProduct.quantity = item.quantity
+        total += product.price * item.quantity
+        deal.products.push(dealProduct)
+    }
+	
+    deal.total = total
+	var updatedDeal = await Deal.updateOne({ _id: id }, deal)
+	if (updatedDeal.matchedCount === 0) {
+        res.status(404).json({ message: 'Orçamento não encontrado!' })
+        return
     }
     return res.status(200).json({deal, message: 'Orçamento atualizado com sucesso.'});
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao atualizar orçamento" })
+  } catch(error) {
+	console.log(error)
+	res.status(500).send({ message: "Erro ao editar orçamento" })
   }
 });
-*/
 
 module.exports = router;
